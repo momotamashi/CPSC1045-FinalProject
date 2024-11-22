b777110c1834da44cd2111190ff99a260350e61e
Alberto Sanchez <74508412+momotamashi@users.noreply.github.com>
2024-11-22 17:51:16 +0000

    git push

---
+++ finalproject.js
@@ -0,0 +1,7 @@
+// Variables for the game state
+let score = 0;
+let isJumping = false;
+let isDucking = false;
+let character = document.getElementById('character');
+let scoreDisplay = document.getElementById('score');
+let obstaclesContainer = document.getElementById('obstacles');
